d6f54833ae27915bb77facf486d6109fe27cf61c
Andrey Balabokhin <abalabokhin@gmail.com>
2015-10-31 17:22:17 -0400

    added IDEA project

---
+++ com.grads/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by paladin on 10/31/15.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
