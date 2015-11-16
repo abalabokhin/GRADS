eae40edd4ade35c282e3c5ac952d54d36db82d86
Andrey Balabokhin <abalabokhin@gmail.com>
2015-11-16 07:34:52 -0500

    added comment with all possible milestones. Milestones need to be refactored from string to enum. To check them better.

--- grads/src/edu/sc/csce740/model/Milestone.java
+++ grads/src/edu/sc/csce740/model/Milestone.java
@@ -5,6 +5,35 @@ package edu.sc.csce740.model;
  */
 public class Milestone
 {
+    enum MilestoneType {
+
+        DISSERTATION_ADVISOR_SELECTED,
+        DISSERTATION_COMMITTEE_FORMED
+
+        /// TODO: Add more:
+//        Program of Study Submitted
+//        Dissertation Advisor Selected
+//        Program of Study Submitted
+//        Dissertation Committee Formed
+//        Qualifying Exam Passed
+//        Dissertation Proposal Scheduled
+//        Comprehensive Exam Passed
+//        Dissertation Submitted
+//        Dissertation Defense Scheduled
+//        Dissertation Defense Passed
+//
+//        Academic Advisor Appointed
+//        Thesis Advisor Selected
+//        Thesis Committee Formed
+//        Thesis Proposal Scheduled
+//        Thesis Proposal Approved
+//        Thesis Submitted
+//        Thesis Defense Scheduled
+//        Thesis Defense Passed
+//
+//        Report Submitted
+//        Report Approved
+        }
     public String milestone;
     public Term term;
 }
