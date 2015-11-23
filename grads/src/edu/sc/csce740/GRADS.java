bf7be3b26d58b9b3909c21426982588b3fd05f2d
amd2amd <knj_7@yahoo.com>
2015-11-23 14:03:57 -0500

    commented not working code

--- grads/src/edu/sc/csce740/GRADS.java
+++ grads/src/edu/sc/csce740/GRADS.java
@@ -140,16 +140,16 @@ public class GRADS implements GRADSIntf
     {
      	checkAuthorization(RequestType.UPDATE_TRANSCRIPT, userId);
         if (permanent) {
-                try{
-                    if(userId.type != "GPC")
-                    {
-                        System.out.println("User is not eligible to update fields");
-                    }
-                  }
-                catch(Exception IE)
-                {
-                    throw new userTypeMismatchException();
-                }
+//                try{
+//                    if(userId != "GPC")
+//                    {
+//                        System.out.println("User is not eligible to update fields");
+//                    }
+//                  }
+//                catch(Exception IE)
+//                {
+//                    throw new userTypeMismatchException();
+//                }
             String finalTranscript = new GsonBuilder().setPrettyPrinting().create().toJson(transcript);
             /// TODO: throw exception if fields cannot be updated by logged user type.
             /// TODO: change db if updating is permanent
